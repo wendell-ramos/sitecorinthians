a6d09846013d37ebd3b0e00f89e3409e38e16212
wendell-ramos <lunascimentoramos21@gmail.com>
2025-08-05 21:17:08 -0300

    Update login.js

--- login.js
+++ login.js
@@ -7,27 +7,21 @@ function logar(){
     if(email.trim()==="" || senha.trim()==="")
        document.getElementById('msg').innerText = "Informar e-mail e senha";
     else{
-         //reccuperar array de usuarios da storage
          let array_usuarios = JSON.parse(
              localStorage.getItem("array_usuarios"));
  
          if(array_usuarios==null)
              document.getElementById('msg').innerText = "Não é possivel validar! Cadastre-se";
          else{
-             //validar (filtro no array procurando quem tem o email e senha iguais aos digitados)
              let usuario = array_usuarios.filter(p => p.senha == senha && p.email == email);
          
-             if(usuario.length>0) //encontrou alguem com estes dados
+             if(usuario.length>0) 
              {
-                 //dados estão corretos
-                 //salvar o nome do usuario na storage
                  localStorage.setItem("key_email", usuario[0].nome);
- 
-                 //redirecionar o usuario para a pagina interna
+
                  location.href='paginainterna.html';
  
-             }
-             //document.getElementById('msg').innerText = "Dados corretos";   
+             } 
              else
              document.getElementById('msg').innerText = "Login não encontrado! Cadastre-se!";   
          }
@@ -35,4 +29,5 @@ function logar(){
  }
  function cadastrar(){
     location.href='cadastro.html';
+
  }
